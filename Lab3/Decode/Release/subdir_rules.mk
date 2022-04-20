95fe49d1a752cbfd26ee8d43159b39a4136b7436
jiaweizheng1 <59036461+jiaweizheng1@users.noreply.github.com>
2022-04-20 12:51:42 -0700

    Add files via upload

---
+++ Lab3/Decode/Release/subdir_rules.mk
@@ -0,0 +1,22 @@
+################################################################################
+# Automatically-generated file. Do not edit!
+################################################################################
+
+SHELL = cmd.exe
+
+# Each subdirectory must supply rules for building sources it contributes
+%.obj: ../%.c $(GEN_OPTS) | $(GEN_FILES)
+	@echo 'Building file: "$<"'
+	@echo 'Invoking: ARM Compiler'
+	"C:/ti.old/bin/armcl" -mv7M4 --code_state=16 --float_support=fpalib -me -Ooff --include_path="C:/ti.old/include" --include_path="C:/ti/CC3200SDK_1.4.0/cc3200-sdk/example/common" --include_path="C:/ti/CC3200SDK_1.4.0/cc3200-sdk/driverlib" --include_path="C:/ti/CC3200SDK_1.4.0/cc3200-sdk/inc" --define=ccs --define=NON_NETWORK --define=cc3200 -g --gcc --diag_warning=225 --diag_wrap=off --display_error_number --abi=eabi --preproc_with_compile --preproc_dependency="$(basename $(<F)).d_raw" $(GEN_OPTS__FLAG) "$<"
+	@echo 'Finished building: "$<"'
+	@echo ' '
+
+startup_ccs.obj: C:/ti/CC3200SDK_1.4.0/cc3200-sdk/example/common/startup_ccs.c $(GEN_OPTS) | $(GEN_FILES)
+	@echo 'Building file: "$<"'
+	@echo 'Invoking: ARM Compiler'
+	"C:/ti.old/bin/armcl" -mv7M4 --code_state=16 --float_support=fpalib -me -Ooff --include_path="C:/ti.old/include" --include_path="C:/ti/CC3200SDK_1.4.0/cc3200-sdk/example/common" --include_path="C:/ti/CC3200SDK_1.4.0/cc3200-sdk/driverlib" --include_path="C:/ti/CC3200SDK_1.4.0/cc3200-sdk/inc" --define=ccs --define=NON_NETWORK --define=cc3200 -g --gcc --diag_warning=225 --diag_wrap=off --display_error_number --abi=eabi --preproc_with_compile --preproc_dependency="$(basename $(<F)).d_raw" $(GEN_OPTS__FLAG) "$<"
+	@echo 'Finished building: "$<"'
+	@echo ' '
+
+
